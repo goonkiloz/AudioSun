dcf83144f30ca6eee82276693f3d40b06ba8cda8
Zhongyuan Ding <zhongyuan.d@gmail.com>
2024-02-06 19:53:41 -0500

    revised song table with correct foreign key.

--- app/models/song.py
+++ app/models/song.py
@@ -11,8 +11,8 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 playlist_songs = db.Table(
     'playlist_songs',
     db.Model.metadata,
-    db.Column('playlist_id', db.Integer, db.ForeignKey('playlists.id'), primary_key=True),
-    db.Column('song_id', db.Integer, db.ForeignKey('songs.id'), primary_key=True)
+    db.Column('playlist_id', db.Integer, db.ForeignKey(add_prefix_for_prod('playlists.id')), primary_key=True),
+    db.Column('song_id', db.Integer, db.ForeignKey(add_prefix_for_prod('songs.id')), primary_key=True)
 
 )
 
