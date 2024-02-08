18d725b7e377ef8618432e717fdd6b5a28c939e1
urfavoritejoel <yourfavoritejoel@gmail.com>
2024-02-07 18:29:23 -0600

    finished seeder file for 20 songs

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .songs import seed_songs, undo_songs
 
 from app.models.db import db, environment, SCHEMA
 
@@ -17,7 +18,9 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users() #am i supposed to change that?
+        undo_songs()
     seed_users()
+    seed_songs()
     # Add other seed functions here
 
 
@@ -25,4 +28,5 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_songs()
     # Add other undo functions here
