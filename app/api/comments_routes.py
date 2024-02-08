21f8e74c9f72391a108aea47cf9a949d21b4590c
Zhongyuan Ding <zhongyuan.d@gmail.com>
2024-02-08 16:07:02 -0500

    edited song route.

--- app/api/comments_routes.py
+++ app/api/comments_routes.py
@@ -60,42 +60,4 @@ def delete_comment(comment_id):
 
 
 
-#Eddie GET comments from a song
-@song_routes.route('/<int:song_id>/comments', methods=['GET'])
-def get_comments_for_song(song_id):
-    """
-    Query for all comments based on the song Id and returns the comments in a list
-    """
-    all_comments = Comment.query.filter(Comment.song_id == song_id).order_by(Comment.updated_at).all()
 
-    if not all_comments:
-        return {'error': 'no comment is found'}, 404
-
-    return {'comments': [comment.to_dict() for comment in all_comments]}
-
-#Eddie Add a comment to a song
-@song_routes.route('/<int:song_id>/comments', methods=['POST'])
-@login_required
-def add_comments_for_song(song_id):
-    """
-    add a comment based on the song Id and user Id
-    """
-    current_song = Song.query.get(song_id)
-
-    if current_user.id != current_song.user_id:
-        return {'error': 'Not Authorized'}, 403
-
-    form = NewCommentForm()
-    form['csrf_token'].data = request.cookies['csrf_token']
-    #how do i test for this in postman??????
-    if form.validate_on_submit():
-        new_comment = Comment(
-            comment_text=form.data['comment_text'],
-            user_id=current_user.id, 
-            song_id=song_id
-        )
-        db.session.add(new_comment)
-        db.session.commit()
-        return new_comment.to_dict()
-
-    return form.errors, 401
