056b6d0cafa1bd8cbe1c72bb892a8490be3a9c07
Zhongyuan Ding <zhongyuan.d@gmail.com>
2024-02-13 13:47:11 -0500

    started comment frontend.

---
+++ react-vite/src/Screens/Comments/RemoveCommentModal/RemoveCommentModal.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useModal } from '../../../context/Modal';
+import './RemoveCommentModal.css'
+import { deleteCommentThunk } from '../../../redux/comments';
+
+function RemoveComment ({commentId, songId}) {
+    //console.log(spotId)
+    const dispatch = useDispatch();
+    //const sessionUser = useSelector((state) => state.session.user);
+
+    const [errors, setErrors] = useState({});
+    const { closeModal } = useModal();
+    //console.log(spotId)
+    const handleConfirmSubmit = (e) => {
+        e.preventDefault();
+        setErrors({});
+
+        return dispatch(deleteCommentThunk(commentId, songId))
+          .then(closeModal)
+          .catch(async (res) => {
+            //const data = await res.json();
+            //console.log(data)
+            if (res && res.message) {
+                setErrors(res);
+            }
+            //console.log(errors)
+        });
+    };
+    const handleCancelSubmit = (e) => {
+        e.preventDefault();
+        closeModal()
+    };
+
+
+    return (
+        <div className='deletespot-container'>
+            <h1>Confirm Delete</h1>
+
+            {errors.message && (
+                <p className=''>{errors.message}</p>
+            )}
+
+            <p>
+                Are you sure you want to remove this comment?
+            </p>
+
+            <button
+                className='delete-comment-confirm-button'
+                type='button'
+                onClick={handleConfirmSubmit}
+            >
+                Yes (Delete Comment)
+            </button>
+
+            <button
+                className='delete-comment-cancel-button'
+                type='button'
+                onClick={handleCancelSubmit}
+            >
+                No (Keep Comment)
+            </button>
+
+        </div>
+    )
+}
+
+export default RemoveComment
--- react-vite/src/Screens/Songs/AllSongs/SingleSongView.jsx
+++ react-vite/src/Screens/Songs/AllSongs/SingleSongView.jsx
@@ -1,12 +1,22 @@
 import "./SongsView.css"
+import CommentsView from "../../Comments/AllComments/CommentsView"
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-const SingleSongView = (song) => {
-    song = song.song
-    if (!song) return <h2>Loading...</h2>
+const SingleSongView = () => {
+    const {songId} = useParams();
+    const currentSong = useSelector(state => state.songs.byId[songId])
+    console.log(currentSong)
+
+    if (!currentSong) return <h2>Loading...</h2>
 
     return (
         <div className="songContainer">
-            {song.title}
+            <h2>Title: {currentSong.title}</h2>
+            <div>Description: {currentSong.description}</div>
+            <div>Genre: {currentSong.genre}</div>
+            <div>Artist: {currentSong.user_id}</div>
+            <CommentsView song={currentSong} />
         </div>
     )
 }
