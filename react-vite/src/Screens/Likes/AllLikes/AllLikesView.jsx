2bed01ce9e172a494a0217cdc73731f7c044114c
Zhongyuan Ding <zhongyuan.d@gmail.com>
2024-02-15 13:49:41 -0500

    finished like or removelike and comment out playlist.

--- react-vite/src/Screens/Likes/AllLikes/AllLikesView.jsx
+++ react-vite/src/Screens/Likes/AllLikes/AllLikesView.jsx
@@ -2,7 +2,6 @@ import './AllLikes.css';
 import { getLikesThunk } from "../../../redux/likes";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import LikeOrRemoveLike from './LikeOrRemoveLike';
 
 const AllLikesView = (song) => {
 
