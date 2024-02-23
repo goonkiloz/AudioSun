d63e47e682e497a2d01db102889a6565cf4be58a
Zhongyuan Ding <zhongyuan.d@gmail.com>
2024-02-22 20:49:37 -0500

    fixed white space not showing up error for all songs and playlist forms.

--- app/forms/edit_playlist_form.py
+++ app/forms/edit_playlist_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Length
 
 
 # def url_check(form, self):
@@ -10,5 +10,25 @@ from wtforms.validators import DataRequired
 
 
 class EditPlaylistForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    description = StringField('Description', validators=[DataRequired()])
+    def no_white_space(FlaskForm, field):
+        if field.data and field.data.startswith(" "):
+            raise ValidationError("Content should not start with whitespace.")
+        if field.data and field.data.endswith(" "):
+            raise ValidationError("Content should not end with whitespace.")
+
+    title = StringField(
+        "Title",
+        validators=[
+            DataRequired(),
+            Length(max=50, message="Title cannot be longer than 50 characters"),
+            no_white_space,
+        ],
+    )
+    description = StringField(
+        "Description",
+        validators=[
+            DataRequired(),
+            no_white_space,
+            Length(max=255, message="Description cannot be longer than 255 characters"),
+        ],
+    )
