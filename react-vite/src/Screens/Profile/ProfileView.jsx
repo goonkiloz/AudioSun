a3b83691eef0b3d052785e41b0dfe71a2999962f
Zhongyuan Ding <zhongyuan.d@gmail.com>
2024-04-04 09:11:07 -0400

    css for playlistview, singleSongComponent, and profileView.

--- react-vite/src/Screens/Profile/ProfileView.jsx
+++ react-vite/src/Screens/Profile/ProfileView.jsx
@@ -1,7 +1,7 @@
 import { memo, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom";
-import { getCurrentUserSongsThunk } from "../../redux/songs"
+import { getCurrentUserSongsThunk } from "../../redux/songs";
 import OpenModalButton from "../Global/OpenModalButton/OpenModalButton";
 import "./ProfileView.css";
 // import { NavLink } from "react-router-dom";
@@ -10,65 +10,62 @@ import DeleteSong from "../Songs/DeleteSong/DeleteSongModal";
 import EditSong from "../Songs/Edit Song/EditSongModal";
 
 const ProfileView = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.session.user);
-    const songs = useSelector(state => state.songs.currentUserSongs)
-    // const currentSongs = songs.filter(song => song.user_id === user.id);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+  const songs = useSelector((state) => state.songs.currentUserSongs);
+  // const currentSongs = songs.filter(song => song.user_id === user.id);
 
+  useEffect(() => {
+    dispatch(getCurrentUserSongsThunk());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(getCurrentUserSongsThunk());
-    }, [dispatch]);
+  if (!user) return <Navigate to="/login" replace={true} />;
+  // if (!currentSongs) currentSongs = songs.filter(song => song.userId === user.id)
+  if (!songs) return <h1>Loading...</h1>;
 
-    if (!user) return <Navigate to="/login" replace={true} />;
-    // if (!currentSongs) currentSongs = songs.filter(song => song.userId === user.id)
-    if (!songs) return <h1>Loading...</h1>
-
-    if(songs.length === 0) {
-        return (
-            <div>
-                <h1>Songs</h1>
-                <div className="songsContainer">
-                <p>Upload a song!</p>
-                <button
-                className='add-song confirm-button'
-                type='button'
-                onClick={() => {
-                    navigate('/songs/new')
-                }}
-                >Yes</button>
-                </div>
-            </div>
-        )
-    }
-
+  if (songs.length === 0) {
     return (
-        <div className='current-user-songs-container'>
-            <h1>My Songs</h1>
-            <div className="current-user-songs-map">
-                {songs?.map((song) => (
-                    <div key={song.id} className="current-user-song">
-                        <SingleSongComponent song={song} />
-
-                        <div className="current-user-song-buttons">
-
-                            <OpenModalButton
-                                modalComponent={<EditSong song={song}/>}
-                                buttonText={'Edit'}
-                            />
-                            <OpenModalButton
-                                modalComponent={<DeleteSong songId={song.id}/>}
-                                buttonText={'Delete'}
-                            />
-                        </div>
-
-
-                    </div>
-                ))}
-            </div>
+      <div className="current-user-no-song-container">
+        <h1>Songs</h1>
+        <div className="songsContainer">
+          <button
+            className="add-song-confirm-button"
+            type="button"
+            onClick={() => {
+              navigate("/songs/new");
+            }}
+          >
+            Click to upload a song now!
+          </button>
         </div>
-    )
+      </div>
+    );
+  }
+
+  return (
+    <div className="current-user-songs-container">
+      <h1>My Songs</h1>
+      <div className="current-user-songs-map">
+        {songs?.map((song) => (
+          <div key={song.id} className="current-user-song">
+            <SingleSongComponent song={song} />
+
+            <div className="current-user-song-buttons">
+              <OpenModalButton
+                modalComponent={<EditSong song={song} />}
+                buttonText={"Edit"}
+              />
+              <OpenModalButton
+                modalComponent={<DeleteSong songId={song.id} />}
+                buttonText={"Delete"}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default memo(ProfileView);
--- react-vite/src/Screens/Songs/AllSongs/SingleSongComponent.jsx
+++ react-vite/src/Screens/Songs/AllSongs/SingleSongComponent.jsx
@@ -1,125 +1,130 @@
-import "./SingleSongComponent.css"
+import "./SingleSongComponent.css";
 import { useSelector } from "react-redux";
 import { memo, useContext, useEffect, useRef, useState } from "react";
 import { PlayerContext } from "../../../context/PlayerContext";
 import { NavLink, useNavigate } from "react-router-dom";
-import OpenModalButton from '../../Global/OpenModalButton/OpenModalButton';
+import OpenModalButton from "../../Global/OpenModalButton/OpenModalButton";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
 import { FaPlay, FaPause } from "react-icons/fa";
 import { IconContext } from "react-icons";
 import AddSong from "../../Playlist/AddSongModal";
 
 const SingleSongComponent = (song) => {
-    // const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const user = useSelector(state => state.session.user)
-    const { currentSong, setCurrentSong, setIsPlaying, isPlaying, playOneSong } = useContext(PlayerContext);
-    const [isHovering, setIsHovering] = useState(false);
-    const [showMenu, setShowMenu] = useState(false);
-    const ulRef = useRef();
-    song = song.song;
+  // const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.session.user);
+  const { currentSong, setIsPlaying, isPlaying, playOneSong } =
+    useContext(PlayerContext);
+  const [isHovering, setIsHovering] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
+  song = song.song;
 
-    const toggleMenu = (e) => {
-        e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
-        setShowMenu(!showMenu);
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+    setShowMenu(!showMenu);
+  };
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
+  useEffect(() => {
+    if (!isHovering) setShowMenu(false);
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (ulRef.current && !ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
     };
 
-    const handleMouseOver = () => {
-        setIsHovering(true)
-    }
+    document.addEventListener("click", closeMenu);
 
-    const handleMouseOut = () => {
-        setIsHovering(false)
-    }
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu, isHovering]);
 
-    useEffect(() => {
-        if (!isHovering) setShowMenu(false)
-        if (!showMenu) return;
+  // const closeMenu = () => setShowMenu(false);
 
-        const closeMenu = (e) => {
-            if (ulRef.current && !ulRef.current.contains(e.target)) {
-                setShowMenu(false);
-            }
-        };
-
-        document.addEventListener("click", closeMenu);
-
-        return () => document.removeEventListener("click", closeMenu);
-    }, [showMenu, isHovering]);
-
-    // const closeMenu = () => setShowMenu(false);
-
-    return (
-        <div className="songContainer">
-            <div
-                className="img-buttons-container"
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut}
+  return (
+    <div className="songContainer">
+      <div
+        className="img-buttons-container"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        <button
+          className="no-bg-button"
+          onClick={() => navigate(`/songs/${song?.id}`)}
+        >
+          <img src={song?.song_image} className="song-image" />
+        </button>
+        {isHovering && (
+          <>
+            <IconContext.Provider
+              value={{
+                color: "#ff5500",
+                size: "70px",
+              }}
             >
-                <button
-                    className="no-bg-button"
-                    onClick={() => navigate(`/songs/${song?.id}`)}
-                >
-                    <img
-                        src={song?.song_image}
-                        className="song-image"
-                    />
-                </button>
-                {isHovering &&
-                    <>
-                        <IconContext.Provider value={{
-                            color: "#ff5500",
-                            size: "70px"
-                        }}>
-                            <button
-                                className="no-bg-button play-button"
-                                onClick={() => {
-                                    if (!isPlaying || currentSong !== song) {
-                                        playOneSong(song);
-                                    } else {
-                                        setIsPlaying(false);
-                                    }
-                                }}>
-                                {!isPlaying || currentSong !== song ?
-                                    <FaPlay className="play-pause-image" />
-                                    :
-                                    <FaPause className="play-pause-image" />
-                                }
-                            </button>
-                        </IconContext.Provider>
-                        <IconContext.Provider value={{
-                            color: "white",
-                            size: "22px"
-                        }}>
-                            {user &&
-                                <>
-                                    <button className="no-bg-button options-menu"
-                                        onClick={toggleMenu}
-                                        alt={"Options"}>
-                                        <HiOutlineDotsHorizontal />
-                                    </button>
-                                    {showMenu && (
-                                        <ul ref={ulRef} className="options-dropdown">
-                                            <li className="options-buttons">
-                                                <OpenModalButton
-                                                    modalComponent={<AddSong songId={song?.id} />}
-                                                    buttonText={'Add to Playlist'}
-                                                />
-                                            </li>
-                                            <li className="options-buttons">
-                                                <NavLink to={`/songs/${song?.id}`} className={"title"}>
-                                                    <button>Go to Song Page</button>
-                                                </NavLink>
-                                            </li>
-                                        </ul>
-                                    )}
-                                </>
-                            }
-                        </IconContext.Provider>
-                    </>
-                }
-            </div>
-            {/* {user?.id === song?.user_id &&
+              <button
+                className="no-bg-button play-button"
+                onClick={() => {
+                  if (!isPlaying || currentSong !== song) {
+                    playOneSong(song);
+                  } else {
+                    setIsPlaying(false);
+                  }
+                }}
+              >
+                {!isPlaying || currentSong !== song ? (
+                  <FaPlay className="play-pause-image" />
+                ) : (
+                  <FaPause className="play-pause-image" />
+                )}
+              </button>
+            </IconContext.Provider>
+            <IconContext.Provider
+              value={{
+                color: "white",
+                size: "22px",
+              }}
+            >
+              {user && (
+                <>
+                  <button
+                    className="no-bg-button options-menu"
+                    onClick={toggleMenu}
+                    alt={"Options"}
+                  >
+                    <HiOutlineDotsHorizontal />
+                  </button>
+                  {showMenu && (
+                    <ul ref={ulRef} className="options-dropdown">
+                      <li className="options-buttons">
+                        <OpenModalButton
+                          modalComponent={<AddSong songId={song?.id} />}
+                          buttonText={"Add to Playlist"}
+                        />
+                      </li>
+                      <li className="options-buttons">
+                        <NavLink to={`/songs/${song?.id}`} className={"title"}>
+                          <button>Go to Song Page</button>
+                        </NavLink>
+                      </li>
+                    </ul>
+                  )}
+                </>
+              )}
+            </IconContext.Provider>
+          </>
+        )}
+      </div>
+      {/* {user?.id === song?.user_id &&
                 <OpenModalButton
                     modalComponent={<EditSongModal song={song} />}
                     buttonText="Edit Song"
@@ -131,11 +136,10 @@ const SingleSongComponent = (song) => {
                     buttonText="Delete Song"
                 />
             } */}
-            <div>{song?.title}</div>
-            <div>{song?.artist.username}</div>
-        </div>
-    )
-}
-
+      <h3 className="song-title">{song?.title}</h3>
+      <div>{song?.artist.username}</div>
+    </div>
+  );
+};
 
 export default memo(SingleSongComponent);
